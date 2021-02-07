bcb663c5072396c943a8566ef52c7e463d1f475f
Kimberly Hicks <kim@Hicks-MBP.attlocal.net>
2021-02-07 13:37:03 -0500

    Node Lab code update

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const quotes = require("./quotes");
 
-
-// console.log(quotes[index]);
-
 "use strict";
 const http = require("http");
 const port = 8888;
 
 http.createServer((req, res) => {
+
   const index = Math.floor(Math.random() * quotes.length); // random number as the array index
+
   res.writeHead(200, {"Content-type": "text/plain"});
   res.write(quotes[index]);
   res.end();
