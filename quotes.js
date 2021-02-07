e0892ebbf58c5363a3574b5c00ed27fb005c696c
Kimberly Hicks <kim@Hicks-MBP.attlocal.net>
2021-02-07 13:33:22 -0500

    Node Lab code

---
+++ quotes.js
@@ -0,0 +1,14 @@
+const quotes = [
+    "Computers are fast; programmers keep it slow.", 
+    "When I wrote this code, only God and I understood what I did. Now only God knows.",
+    "A son asked his father (a programmer) why the sun rises in the east, and sets in the west. His response? It works, don't touch!",
+    "Copy-and-Paste was programmed by programmers for programmers actually.",
+    "Algorithm: Word used by programmers when they don't want to explain what they did.",
+    "If debugging is the process of removing bugs, then programming must be the process of putting them in.",
+    "99 little bugs in the code. 99 little bugs in the code. Take one down, patch it around. 127 little bugs in the code...",
+    "!false  // (It's funny because it's true.)",
+    "['hip','hip'] (hip hip array!)",
+    "Why did the programmer quit her job? Because she didn't get arrays."
+]; 
+
+module.exports = quotes;
